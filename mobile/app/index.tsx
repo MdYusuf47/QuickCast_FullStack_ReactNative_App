bea10c02635cb765167c6c3edf9fcbab3e866d96
Md Yusuf <mdyusuf742171@gmail.com>
2025-07-29 16:00:31 +0530

    add custom hook for social authentication using Clerk SSO

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,10 @@
-import { Text, View } from "react-native";
+import { View, Text } from 'react-native'
+import React from 'react'
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
-      </Text>
+    <View>
+      <Text>index</Text>
     </View>
-  );
+  )
 }
